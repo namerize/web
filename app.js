44f1748e2c8bed2fb9d03f03680718f2a774188c
JoshG <josh@onezero.hk>
2015-10-18 12:55:55 +0800

    added changes to MessageCTRL

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 var app = angular.module("namerize", ["firebase"]);
 
-app.controller("SampleCtrl", function($scope, $firebaseObject) {
-  var ref = new Firebase("https://namerize.firebaseio.com/data");
+app.controller("MessageCtrl", function($scope, $firebaseArray) {
+  var ref = new Firebase("https://namerize.firebaseio.com/messages");
 
-  // download the data into a local object
-  var syncObject = $firebaseObject(ref);
+  // create a synchronized array
+  $scope.messages = $firebaseArray(ref);
 
-  // synchronize the object with a three-way data binding
-  // click on `index.html` above to see it used in the DOM!
-  syncObject.$bindTo($scope, "data");
+  // add new items to the array
+  // the message is automatically added to our Firebase database!
+  $scope.addMessage = function() {
+    $scope.messages.$add({
+      text: $scope.newMessageText
+    });
+  };
+
+  // click on `index.html` above to see $remove() and $save() in action
 });
+
+
+
+
